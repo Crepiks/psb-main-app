704057b0a51c3c9e2224288f9ed91dfb6a7362b7
Crepiks <sayazhan.onlassyn@mail.ru>
2021-06-06 01:50:23 +0600

    feat: create markup of home page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import "package:micro_mobile/micro_mobile.dart";
 import "package:payments/app.dart";
 import "package:profile/app.dart";
+import './models/transaction_model.dart';
+import "./widgets/transaction_card.dart";
 
 void main() {
   runApp(App());
@@ -21,19 +23,19 @@ class App extends StatelessWidget {
       print('TRANSACTION_ADD');
     });
 
-    store.addEventListener('NAVIGATE_HOME', () {
+    store.addEventListener('NAVIGATE_HOME', (BuildContext context) {
       print('NAVIGATE_HOME');
     });
 
-    store.addEventListener('NAVIGATE_PROFILE', () {
+    store.addEventListener('NAVIGATE_PROFILE', (BuildContext context) {
       print('NAVIGATE_PROFILE');
     });
 
-    store.addEventListener('NAVIGATE_PAYMENTS', () {
+    store.addEventListener('NAVIGATE_PAYMENTS', (BuildContext context) {
       print('NAVIGATE_PAYMENTS');
     });
 
-    store.addEventListener('NAVIGATE_POP', () {
+    store.addEventListener('NAVIGATE_POP', (BuildContext context) {
       print('NAVIGATE_POP');
     });
   }
@@ -45,38 +47,113 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Screen(
+      home: HomeScreen(
         store: store,
       ),
     );
   }
 }
 
-class Screen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   final Store store;
 
-  Screen({Key? key, required this.store}) : super(key: key);
+  HomeScreen({Key? key, required this.store}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final transactions = <Transaction>[
+    Transaction(name: "Hekki", date: "06.05.2021", value: "+250 USA")
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    widget.store.addEventListener("TRANSACTION_ADD", (Transaction payload) {
+      addTransation(payload);
+      addTransation(
+          Transaction(name: "Hello", date: "06.05.2021", value: "+250 USA"));
+    });
+  }
+
+  void addTransation(Transaction transaction) {
+    setState(() {
+      transactions.add(transaction);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    store.addEventListener('NAVIGATE_PROFILE', () {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => ProfileScreen(store: store)));
-    });
-
-    store.addEventListener('NAVIGATE_PAYMENTS', () {
+    widget.store.addEventListener('NAVIGATE_PROFILE', (BuildContext context) {
       Navigator.push(
           context,
           MaterialPageRoute(
-              builder: (context) => PaymentsScreen(store: store)));
+              builder: (context) => ProfileScreen(store: widget.store)));
     });
 
-    store.addEventListener('NAVIGATE_POP', () {
+    widget.store.addEventListener('NAVIGATE_PAYMENTS', (BuildContext context) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => PaymentsScreen(store: widget.store)));
+    });
+
+    widget.store.addEventListener('NAVIGATE_POP', (BuildContext context) {
       Navigator.pop(context);
     });
 
-    return PaymentsScreen(
-      store: store,
-    );
+    return Scaffold(
+        body: SafeArea(
+            child: Padding(
+      padding: EdgeInsets.only(right: 20, left: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    widget.store.emit('NAVIGATE_PAYMENTS', context);
+                  },
+                  icon: Icon(Icons.credit_card)),
+              Container(
+                height: 70,
+                alignment: Alignment.center,
+                child: Text(
+                  "Главная",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+              ),
+              IconButton(
+                  onPressed: () {
+                    widget.store.emit('NAVIGATE_PROFILE', context);
+                  },
+                  icon: Icon(Icons.person))
+            ],
+          ),
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: ListView.builder(
+                    itemCount: transactions.length,
+                    itemBuilder: (context, index) {
+                      var transaction = transactions[index];
+
+                      return Padding(
+                        padding: EdgeInsets.only(top: 10, bottom: 10),
+                        child: TransactionCard(
+                            name: transaction.name,
+                            date: transaction.date,
+                            value: transaction.value),
+                      );
+                    })),
+          ),
+        ],
+      ),
+    )));
   }
 }
